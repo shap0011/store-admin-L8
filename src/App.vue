d5e1452ebdd2d5927d380d3710ad5d31e1f56f18
shap0011 <shap0011@algonquinlive.com>
2025-12-10 21:42:42 -0500

    Change style

--- src/App.vue
+++ src/App.vue
@@ -134,7 +134,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
   margin-top: 120px;
   padding: 1rem;
 }
